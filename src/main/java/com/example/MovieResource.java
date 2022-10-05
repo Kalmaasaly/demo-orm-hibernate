ce89a1813bfe233338e9ed42e2c1f60ce7bc4c25
Kalmaasaly <kalmaasaly@gmail.com>
2022-10-05 23:16:57 +0300

    implements metrics add Counted method

--- src/main/java/com/example/MovieResource.java
+++ src/main/java/com/example/MovieResource.java
@@ -2,6 +2,7 @@ package com.example;
 
 
 
+import org.eclipse.microprofile.metrics.MetricUnits;
 import org.eclipse.microprofile.metrics.annotation.Counted;
 import org.eclipse.microprofile.metrics.annotation.Timed;
 
@@ -19,7 +20,10 @@ public class MovieResource {
     @GET
     @Produces(MediaType.APPLICATION_JSON)
     @Counted(name = "get all movies",displayName = "getAllMovie",description = "fetch  all movies")
-    @Timed(name = " fetched time movie by Id ",displayName = "fetchTimeMovieById",description = "fetch time of movie by Id")
+    @Timed(name = " fetched time movie by Id ",
+            displayName = "fetchTimeMovieById",
+            description = "fetch time of movie by Id",
+            unit = MetricUnits.MILLISECONDS )
     public Response getAll() {
         List<Movie> movieList=Movie.listAll();
         return Response.ok(movieList).build();
@@ -28,7 +32,10 @@ public class MovieResource {
     @Path("{id}")
     @Produces(MediaType.APPLICATION_JSON)
     @Counted(name = "get movie by Id",displayName = "getById",description = "fetch  movie match Id")
-    @Timed(name = " fetched time of movie by Id ",displayName = "fetchTimeMovieById",description = "fetch time of movie by Id")
+    @Timed(name = " fetched time of movie by Id ",
+            displayName = "fetchTimeMovieById",
+            description = "fetch time of movie by Id",
+    unit = MetricUnits.MILLISECONDS)
     public Response getById(@PathParam("id") Long id) {
         var movieO=Movie.findByIdOptional(id);
         return movieO.map(movie->Response.ok(movie).build())
@@ -40,7 +47,10 @@ public class MovieResource {
     @Path("country/{country}")
     @Produces(MediaType.APPLICATION_JSON)
     @Counted(name = "get movie by country",displayName = "getByCountry",description = "fetch all movie match country name")
-    @Timed(name = " fetched time of movie by Country ",displayName = "fetchTimeMovieByCountry",description = "fetch time of movie by country")
+    @Timed(name = " fetched time of movie by Country ",
+            displayName = "fetchTimeMovieByCountry",
+            description = "fetch time of movie by country",
+            unit = MetricUnits.MILLISECONDS)
     public Response getByCountry(@PathParam("country") String country) {
         var movies=Movie.list("select u from Movie u where u.country=?1",country);
         return Response.ok(movies).build();
@@ -50,7 +60,10 @@ public class MovieResource {
     @Path("title/{title}")
     @Produces(MediaType.APPLICATION_JSON)
     @Counted(name = "get movie by title",displayName = "getByTitle",description = "fetch all movie match title")
-    @Timed(name = " fetched time of movie by Title ",displayName = "fetchTimeMovieByTitle",description = "fetch time of movie by title")
+    @Timed(name = " fetched time of movie by Title ",
+            displayName = "fetchTimeMovieByTitle",
+            description = "fetch time of movie by title",
+            unit = MetricUnits.MILLISECONDS)
     public Response getByTitle(@PathParam("title") String title) {
         var movieO=Movie.find("title",title).singleResultOptional();
         return movieO.map(movie->Response.ok(movie).build())
@@ -61,7 +74,9 @@ public class MovieResource {
     @Transactional
     @Produces(MediaType.APPLICATION_JSON)
     @Consumes(MediaType.APPLICATION_JSON)
-    @Counted(name = "create movie ",displayName = "insertNewMovie",description = "insert a new movie into db")
+    @Counted(name = "create movie ",displayName = "insertNewMovie",
+            description = "insert a new movie into db",
+            unit = MetricUnits.MILLISECONDS)
     @Timed(name = " created time of movie ",displayName = "timeCreatedMovie",description = "created time of movie by Id")
     public Response create(Movie movie) {
        Movie.persist(movie);
@@ -76,7 +91,10 @@ public class MovieResource {
     @Path("{id}")
     @Produces(MediaType.APPLICATION_JSON)
     @Counted(name = "delete movie ",displayName = "deleteMovie",description = "delete movie by Id")
-    @Timed(name = " deleted time of movie ",displayName = "timedeleteMovie",description = "delete time of movie by Id")
+    @Timed(name = " deleted time of movie ",
+            displayName = "timedeleteMovie",
+            description = "delete time of movie by Id",
+            unit = MetricUnits.MILLISECONDS)
     public Response delete(@PathParam("id") Long id){
        if (Movie.deleteById(id)){
            return Response.noContent().build();
